9993620532d9a7239c00415ddb5c897a529636e8
aaa <aaa@aaa.aaa>
2026-01-03 15:45:41 +0700

    add_theming

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-
 import Button from '@mui/material/Button'
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
 import Settings from '@mui/icons-material/Settings'
+import Typography from '@mui/material/Typography'
+
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
       <div>PeaIsPea</div>
+      <Typography variant='body2' color="text.secondary">Tao là bố mày</Typography>
       <Button variant="contained">Hello world</Button>
       <AccessAlarmIcon></AccessAlarmIcon>
       <ThreeDRotation></ThreeDRotation>
