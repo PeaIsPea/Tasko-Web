0ee796d748a097782cae2ca08089c0f550a0d7b6
aaa <aaa@aaa.aaa>
2026-01-05 16:24:29 +0700

    Layout Basic

---
+++ src/pages/Users/_id.jsx
@@ -0,0 +1,1 @@
+// uSer details
--- src/theme.js
+++ src/theme.js
@@ -5,18 +5,25 @@ import { cyan, deepOrange, orange, teal } from '@mui/material/colors'
 
 // Create a theme instance.
 const theme = extendTheme({
+  taskoCustom:{
+    appBarHeight: '48px',
+    boardBarHeight: '58px'
+
+  },
   colorSchemes: {
     light: {
       palette: {
         primary: teal,
         secondary: deepOrange
       }
+
     },
     dark: {
       palette: {
         primary: cyan,
         secondary: orange
       }
+
     }
   }
   // ...other properties
