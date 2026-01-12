b3ccb49676fed596d2282d94ddfe4426206be0e6
aaa <aaa@aaa.aaa>
2026-01-12 12:51:33 +0700

    drag_drop_col

--- src/pages/Boards/BoardContent/ListCols/ListCols.jsx
+++ src/pages/Boards/BoardContent/ListCols/ListCols.jsx
@@ -3,42 +3,45 @@ import Box from '@mui/material/Box'
 import Col from './Col/Col'
 import { Button } from '@mui/material'
 import AddToPhotosIcon from '@mui/icons-material/AddToPhotos'
+import { SortableContext, horizontalListSortingStrategy } from '@dnd-kit/sortable'
 
 function ListCols({ columns }) {
   return (
-    <Box sx={{
-      bgcolor: 'inherit',
-      width: '100%',
-      height: '100%',
-      display: 'flex',
-      overflowX: 'auto',
-      overflowY: 'hidden',
-      '&::-webkit-scrollbar-track': {
-        m: 2
-      }
-    }}>
-      {columns?.map((column) => {
-        return (
-          <Col key={column?._id} column={column} />
-        )
-      })}
-
-      {/* Button Add new col*/}
+    <SortableContext items={columns?.map(c => c._id)} strategy={horizontalListSortingStrategy}>
       <Box sx={{
-        minWidth: '200px',
-        maxWidth: '200px',
-        mx: 2,
-        borderRadius: '6px',
-        height: 'fit-content',
-        bgcolor: '#ffffff3c'
+        bgcolor: 'inherit',
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        overflowX: 'auto',
+        overflowY: 'hidden',
+        '&::-webkit-scrollbar-track': {
+          m: 2
+        }
       }}>
-        <Button
-          startIcon={<AddToPhotosIcon />}
-          sx={{ color: 'white', width: '100%', justifyContent: 'flex-start', pl: 2.5, py: 1 }}
-        >Add new column</Button>
-      </Box>
+        {columns?.map((column) => {
+          return (
+            <Col key={column?._id} column={column} />
+          )
+        })}
 
-    </Box>
+        {/* Button Add new col*/}
+        <Box sx={{
+          minWidth: '200px',
+          maxWidth: '200px',
+          mx: 2,
+          borderRadius: '6px',
+          height: 'fit-content',
+          bgcolor: '#ffffff3c'
+        }}>
+          <Button
+            startIcon={<AddToPhotosIcon />}
+            sx={{ color: 'white', width: '100%', justifyContent: 'flex-start', pl: 2.5, py: 1 }}
+          >Add new column</Button>
+        </Box>
+
+      </Box>
+    </SortableContext>
   )
 }
 
